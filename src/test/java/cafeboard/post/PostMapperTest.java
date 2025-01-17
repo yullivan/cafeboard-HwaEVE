e106368ee8df27b0e112437ab5bab6c30892fcfe
Kimmy <kimsungrak00@gmail.com>
2025-01-17 16:43:31 +0900

    [MyBatis] 설정 및 테스트

---
+++ src/test/java/cafeboard/post/PostMapperTest.java
@@ -0,0 +1,18 @@
+package cafeboard.post;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class PostMapperTest{
+
+    @Autowired
+    PostMapper postMapper;
+
+    @Test
+    void name() {
+        postMapper.findAll(null);
+        postMapper.findAll(3L);
+    }
+}
